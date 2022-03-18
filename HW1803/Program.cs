8629e5593dbe66c0d0f8f92ece3165e0ea7e44d4
validash <100703058+validash@users.noreply.github.com>
2022-03-18 11:32:39 +0400

    created HW1803 file

---
+++ HW1803/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace HW1803
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TASK1
+            //    int[] array = { 7, 9, 11, 23 };
+            //    int val = 27;
+            //    Console.WriteLine(AddedElementToArray( ref array, val));
+            //} 
+            //static void AddedElementToArray( ref int[] array,int number)
+            //{ for(int i=0;i<array.Length;i++)
+            //    { for (int j = 0; j < array.Length + 1; i++)
+            //        {
+            //            int[] newArray = new int[array.Length + 1];
+            //            while (array[i] == newArray[j])
+            //            {
+            //                newArray += array[i]+{number};
+            //            }
+            //        }
+            //    }
+
+            //}
+            #endregion
+            #region TASK3
+          //  Console.WriteLine("eded daxil et:");
+          //  int number = Convert.ToInt32(Console.ReadLine());
+          //  Console.WriteLine(FindNearlyRoot ( ref number));
+          //  static int FindNearlyRoot(ref int number)
+          //  {
+          //      int result = 0;
+          //      for(int i = 0; i < number; i++)
+          //      {
+          //          if (i * i <= number) 
+          //          result = i ;
+          //      }
+          //      return result;
+          //  }
+
+
+          //}
+        #endregion
+            #region TASK2
+            //    Console.WriteLine("text daxil edin:");
+            //    string text = Console.ReadLine();
+            //    Console.WriteLine(WithoutSpace (text));
+            //    string withoutSpaceText =(WithoutSpace(text ));
+
+            //}
+            //static string WithoutSpace(string text)
+            //{ for (int i = 0; i < text.Length; i++)
+            //    {
+            //        string withoutSpaceText = "";
+            //        if (text[i] == ' ')
+            //        {
+            //            withoutSpaceText += text[i + 1];
+            //        }
+            //        return withoutSpaceText;
+            //}
+            //    return text;
+            //}
+
+
+            #endregion
+            #region CLASSWORK7
+            //    int n1 = 1567;
+            //    int digitCount = 4;
+            //    Console.WriteLine(ChangeNumber(n1, digitCount));
+
+
+            //}
+            //static int ChangeNumber(int number, int digitCount)
+            //{
+            //    int newNumber = 0;
+            //    int qaliq = 0;
+
+            //    int k = 1;
+            //    int i = 1;
+
+            //    qaliq = number % 10;
+            //    number = (number - qaliq) / 10;
+            //    while (i < digitCount)
+            //    {
+
+            //        k *= 10;
+            //        i++;
+            //    }
+
+            //    newNumber = (qaliq * k) + number;
+
+            //    return newNumber;
+            //}
+
+        }
+    }
+
+    #endregion
+
+
+
+
+ 
